70116490e17bbcb54b497cb786c706d8364f49a4
yunqing <realyunq@gmail.com>
2024-04-01 17:45:51 +0800

    40. combination sum ii #backtrack #pruning

--- com/catxu/leetcode/question40/Solution.java
+++ com/catxu/leetcode/question40/Solution.java
@@ -1,51 +1,53 @@
-package com.catxu.leetcode.question39;
+package com.catxu.leetcode.question40;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
- * 39. Combination Sum
+ * 40. Combination Sum II
  * <p>
- * Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+ * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
  * <p>
- * The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+ * Each number in candidates may only be used once in the combination.
  * <p>
- * The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+ * Note: The solution set must not contain duplicate combinations.
  * <p>
  * <p>
  * <p>
  * Example 1:
  * <p>
- * Input: candidates = [2,3,6,7], target = 7
- * Output: [[2,2,3],[7]]
- * Explanation:
- * 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
- * 7 is a candidate, and 7 = 7.
- * These are the only two combinations.
+ * Input: candidates = [10,1,2,7,6,1,5], target = 8
+ * Output:
+ * [
+ * [1,1,6],
+ * [1,2,5],
+ * [1,7],
+ * [2,6]
+ * ]
  * Example 2:
  * <p>
- * Input: candidates = [2,3,5], target = 8
- * Output: [[2,2,2,2],[2,3,3],[3,5]]
- * Example 3:
- * <p>
- * Input: candidates = [2], target = 1
- * Output: []
+ * Input: candidates = [2,5,2,1,2], target = 5
+ * Output:
+ * [
+ * [1,2,2],
+ * [5]
+ * ]
  * <p>
  * <p>
  * Constraints:
  * <p>
- * 1 <= candidates.length <= 30
- * 2 <= candidates[i] <= 40
- * All elements of candidates are distinct.
- * 1 <= target <= 40
+ * 1 <= candidates.length <= 100
+ * 1 <= candidates[i] <= 50
+ * 1 <= target <= 30
  */
 class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.combinationSum(new int[]{2, 3, 6, 7}, 7));
-        System.out.println(solution.combinationSum(new int[]{2, 3, 5}, 8));
+//        System.out.println(solution.combinationSum(new int[]{2, 3, 6, 7}, 7));
+        System.out.println(solution.combinationSum(new int[]{10, 1, 2, 7, 6, 1, 5}, 8));
+//        System.out.println(solution.combinationSum(new int[]{2, 5, 2, 1, 2}, 5));
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -72,10 +74,13 @@ class Solution {
             if (total + choices[i] > target) {
                 break;
             }
+            if (i > start && choices[i] == choices[i - 1]) {
+                continue;
+            }
             // 尝试：做出选择，更新元素和 total
             state.add(choices[i]);
             // 进行下一轮选择
-            backtrack(state, target, total + choices[i], choices, i, res);
+            backtrack(state, target, total + choices[i], choices, i + 1, res);
             // 回退：撤销选择，恢复到之前的状态
             state.remove(state.size() - 1);
         }
