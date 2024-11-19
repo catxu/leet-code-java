64984018aa4d21880269fbc589583b3ad92f105f
yunqing <realyunq@gmail.com>
2024-11-19 08:48:34 +0800

    128. Longest Consecutive Sequence

---
+++ com/catxu/leetcode/question128/Solution.java
@@ -0,0 +1,68 @@
+package com.catxu.leetcode.question128;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 128. Longest Consecutive Sequence
+ * <p>
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * <p>
+ * You must write an algorithm that runs in O(n) time.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [100,4,200,1,3,2]
+ * <p>
+ * Output: 4
+ * <p>
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * <p>
+ * Output: 9
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= nums.length <= 10^5
+ * <p>
+ * -10^9 <= nums[i] <= 10^9
+ */
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        Set<Integer> set = new TreeSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        Iterator<Integer> iterator = set.iterator();
+        int result = 1;
+        int cur = 1;
+        int prev = Integer.MIN_VALUE;
+        while (iterator.hasNext()) {
+            Integer num = iterator.next();
+            if (num - 1 == prev) {
+                // 计算连续
+                cur++;
+                result = Math.max(cur, result);
+            } else {
+                cur = 1;
+            }
+            prev = num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
+        System.out.println(s.longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+        System.out.println(s.longestConsecutive(new int[]{0}));
+        System.out.println(s.longestConsecutive(new int[]{}));
+    }
+}
--- com/catxu/leetcode/question88/Solution.java
+++ com/catxu/leetcode/question88/Solution.java
@@ -2,6 +2,58 @@ package com.catxu.leetcode.question88;
 
 /**
  * 88. Merge Sorted Array
+ * <p>
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * <p>
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * <p>
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * <p>
+ * Output: [1,2,2,3,5,6]
+ * <p>
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * <p>
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * <p>
+ * Output: [1]
+ * <p>
+ * Explanation: The arrays we are merging are [1] and [].
+ * <p>
+ * The result of the merge is [1].
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ * <p>
+ * Output: [1]
+ * <p>
+ * Explanation: The arrays we are merging are [] and [1].
+ * <p>
+ * The result of the merge is [1].
+ * <p>
+ * Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ * <p>
+ * Constraints:
+ * <p>
+ * nums1.length == m + n
+ * <p>
+ * nums2.length == n
+ * <p>
+ * 0 <= m, n <= 200
+ * <p>
+ * 1 <= m + n <= 200
+ * <p>
+ * -10^9 <= nums1[i], nums2[j] <= 10^9
+ * <p>
+ * Follow up: Can you come up with an algorithm that runs in O(m + n) time?
  */
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -19,13 +71,10 @@ class Solution {
                 return;
             }
             if (nums1[m] < nums2[n]) {
-                nums1[end] = nums2[n];
-                n--;
+                nums1[end--] = nums2[n--];
             } else {
-                nums1[end] = nums1[m];
-                m--;
+                nums1[end--] = nums1[m--];
             }
-            end--;
         }
     }
 
