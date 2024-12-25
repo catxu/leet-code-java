d8083802452b6befcc02b023df4b39555d9842e9
catxu <catxu@foxmail.com>
2024-12-25 23:02:35 +0800

    208. Implement Trie (Prefix Tree)

--- com/catxu/leetcode/question208/Trie.java
+++ com/catxu/leetcode/question208/Trie.java
@@ -2,6 +2,56 @@ package com.catxu.leetcode.question208;
 
 import com.catxu.leetcode.question.TrieNode;
 
+/**
+ * 208. Implement Trie (Prefix Tree)
+ * <p>
+ * A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+ * <p>
+ * Implement the Trie class:
+ * <p>
+ * Trie() Initializes the trie object.
+ * <p>
+ * void insert(String word) Inserts the string word into the trie.
+ * <p>
+ * boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+ * <p>
+ * boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input
+ * <p>
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * <p>
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * <p>
+ * Output
+ * <p>
+ * [null, null, true, false, true, null, true]
+ * <p>
+ * Explanation
+ *
+ * <pre>
+ *     {@code
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // return True
+ * trie.search("app");     // return False
+ * trie.startsWith("app"); // return True
+ * trie.insert("app");
+ * trie.search("app");     // return True
+ *     }
+ *
+ * </pre>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= word.length, prefix.length <= 2000
+ * <p>
+ * word and prefix consist only of lowercase English letters.
+ * <p>
+ * At most 3 * 10<sup>4</sup> calls in total will be made to insert, search, and startsWith.
+ */
 class Trie {
     private final TrieNode root;
 
