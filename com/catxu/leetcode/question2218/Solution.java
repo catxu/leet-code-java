89a97030034b5c747734969e2db3ae03b6a4cd50
yunqing <realyunq@gmail.com>
2025-01-22 14:15:54 +0800

    2218. Maximum Value of K Coins From Piles

--- com/catxu/leetcode/question2218/Solution.java
+++ com/catxu/leetcode/question2218/Solution.java
@@ -5,8 +5,73 @@ import java.util.List;
 
 /**
  * 2218. Maximum Value of K Coins From Piles
+ * <p>
+ * There are n piles of coins on a table. Each pile consists of a positive number of coins of assorted denominations.
+ * <p>
+ * In one move, you can choose any coin on top of any pile, remove it, and add it to your wallet.
+ * <p>
+ * Given a list piles, where piles[i] is a list of integers denoting the composition of the ith pile from top to bottom, and a positive integer k, return the maximum total value of coins you can have in your wallet if you choose exactly k coins optimally.
+ * <p>
+ * Example 1:
+ * <pre>
+ * Input: piles = [[1,100,3],[7,8,9]], k = 2
+ * Output: 101
+ * Explanation:
+ * The above diagram shows the different ways we can choose k coins.
+ * The maximum total we can obtain is 101.
+ * </pre>
+ * Example 2:
+ * <pre>
+ * Input: piles = [[100],[100],[100],[100],[100],[100],[1,1,1,1,1,1,700]], k = 7
+ * Output: 706
+ * Explanation:
+ * The maximum total can be obtained if we choose all coins from the last pile.
+ * </pre>
+ * Constraints:
+ * <pre>
+ * n == piles.length
+ * 1 <= n <= 1000
+ * 1 <= piles[i][j] <= 10<sup>5</sup>
+ * 1 <= k <= sum(piles[i].length) <= 2000
+ * </pre>
  */
 class Solution {
+
+    /*
+    Given N items where each item has some weight and profit associated with it and also given a bag with capacity W,
+    [i.e., the bag can hold at most W weight in it].
+    The task is to put the items into the bag such that the sum of profits associated with them is the maximum possible.
+
+    Note: The constraint here is we can either put an item completely into the bag or cannot put it at all
+    [It is not possible to put a part of an item into the bag].
+
+    Input: N = 3, W = 4, profit[] = {1, 2, 3}, weight[] = {4, 5, 1}
+    Output: 3
+    Explanation: There are two items which have weight less than or equal to 4.
+    If we select the item with weight 4, the possible profit is 1.
+    And if we select the item with weight 1, the possible profit is 3.
+    So the maximum possible profit is 3. Note that we cannot put both the items with
+    weight 4 and 1 together as the capacity of the bag is 4.
+
+    Input: N = 3, W = 3, profit[] = {1, 2, 3}, weight[] = {4, 5, 6}
+    Output: 0
+     */
+    public int knapsack(int n, int w, int[] profit, int[] weight) {
+        return dfs(n, w, profit, weight, 0, 0, 0);
+    }
+
+    private int dfs(int n, int w, int[] profit, int[] weight, int start, int curWeight, int curProfit) {
+        if (start >= n || curWeight > w) {
+            return curProfit;
+        }
+        // Option 1: Don't include the current item
+        int maxProfit = dfs(n, w, profit, weight, start + 1, curWeight, curProfit);
+        // Option 2: Include the current item (if weight allows)
+        if (curWeight + weight[start] <= w) {
+            maxProfit = Math.max(maxProfit, dfs(n, w, profit, weight, start + 1, curWeight + weight[start], curProfit + profit[start]));
+        }
+        return maxProfit;
+    }
 
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
         int n = piles.size();
@@ -36,6 +101,9 @@ class Solution {
     }
 
     public static void main(String[] args) {
+        System.out.println(new Solution().knapsack(3, 4, new int[]{1, 2, 3}, new int[]{4, 5, 1}));
+        System.out.println(new Solution().knapsack(3, 3, new int[]{1, 2, 3}, new int[]{4, 5, 6}));
+
         List<Integer> l1_1 = List.of(1, 100, 3);
         List<Integer> l1_2 = List.of(7, 8, 9);
         List<List<Integer>> piles1 = List.of(l1_1, l1_2);
