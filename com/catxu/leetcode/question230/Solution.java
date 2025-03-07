2e45b9387747ef173bfb8bc2d7d5f235febab083
catxu <catxu@foxmail.com>
2025-03-07 23:17:37 +0800

    230. Kth Smallest Element in a BST

---
+++ com/catxu/leetcode/question230/Solution.java
@@ -0,0 +1,32 @@
+package com.catxu.leetcode.question230;
+
+import com.catxu.leetcode.question.TreeNode;
+
+class Solution {
+
+    private int count;
+    private int target;
+
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        inOrder(root);
+        return target;
+    }
+
+    private void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        count--;
+        if (count == 0) {
+            target = root.val;
+            return;
+        }
+        inOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().kthSmallest(TreeNode.levelOrderBuildTree(new Integer[]{3, 1, 4, null, 2}), 1));
+    }
+}
