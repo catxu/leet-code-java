81b4bb10e83e1a30b8c1c41ddc063725b356f8d1
catxu <catxu@foxmail.com>
2025-03-26 07:19:46 +0800

    133. Clone Graph

---
+++ com/catxu/leetcode/question133/Solution.java
@@ -0,0 +1,50 @@
+package com.catxu.leetcode.question133;
+
+import java.util.*;
+
+/**
+ * 133. Clone Graph
+ */
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Map<Node, Node> map = new HashMap<>();
+        map.put(node, new Node(node.val, new ArrayList<>()));
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            for (Node neighbor : cur.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                    queue.offer(neighbor);
+                }
+                map.get(cur).neighbors.add(map.get(neighbor));
+            }
+        }
+
+        return map.get(node);
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
