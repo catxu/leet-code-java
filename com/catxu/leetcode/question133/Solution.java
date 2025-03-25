bfa3c66005b23f4f848c551f01900c200fdfea89
catxu <catxu@foxmail.com>
2025-03-26 07:24:42 +0800

    133. Clone Graph

--- com/catxu/leetcode/question133/Solution.java
+++ com/catxu/leetcode/question133/Solution.java
@@ -28,6 +28,51 @@ class Solution {
         return map.get(node);
     }
 
+    Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraphOptimized(Node node) {
+        // DFS time: O(V + E)
+
+        if (node == null) return null;
+        // if the node was already visited before
+        // return the clone from the visited map
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+        // create a clone for the given node
+        // note that we do not have cloned neighbors as of now, hence[]
+        Node clone = new Node(node.val, new ArrayList<>());
+        // the key is the original node and value is the clone node
+        visited.put(node, clone);
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+        return clone;
+
+        // BFS
+        // if (node == null) return node;
+        // visited = new HashMap<>();
+        // Queue<Node> q = new LinkedList<>();
+        // q.add(node);
+        // visited.put(node, new Node(node.val, new ArrayList<Node>()));
+        // while (!q.isEmpty()) {
+        //     int size = q.size();
+        //     for (int i = 0; i < size; i++) {
+        //         Node curr = q.remove();
+        //         for (Node neighbor: curr.neighbors) {
+        //             if (!visited.containsKey(neighbor)) {
+        //                 visited.put(neighbor, new Node(neighbor.val, new ArrayList<Node>()));
+        //                 q.add(neighbor);
+        //             }
+        //             visited.get(curr).neighbors.add(visited.get(neighbor));
+        //         }
+        //     }
+        // }
+        // return visited.get(node);
+
+    }
+
+
     static class Node {
         public int val;
         public List<Node> neighbors;
