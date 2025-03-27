212f6d618cdd5960467bf939bc8e1d4e8afcb6de
catxu <catxu@foxmail.com>
2025-03-27 22:31:56 +0800

    Dijkstra algorithm

--- com/catxu/leetcode/question743/Solution.java
+++ com/catxu/leetcode/question743/Solution.java
@@ -15,43 +15,40 @@ class Solution {
      * @return delayTime or -1 which cannot cross all nodes
      */
     public int networkDelayTime(int[][] times, int n, int k) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        for (int[] time : times) {
-            // 0: node
-            // 1: neighbor
-            // 2: weight
-            graph.merge(time[0],
-                    new ArrayList<>(List.of(new int[]{time[1], time[2]})),
-                    (oldList, newList) -> {
-                        oldList.addAll(newList);
-                        return oldList;
-                    }
-            );
+        final int INF = Integer.MAX_VALUE / 2;
+        List<int[]>[] graph = new List[n];
+        Arrays.fill(graph, new ArrayList<>());
+        for (int[] route : times) {
+            // 0: source / 1: target / 2: weight
+            int v = route[0] - 1, neighbor = route[1] - 1;
+            graph[v].add(new int[]{neighbor, route[2]});
         }
-        Set<Integer> visited = new HashSet<>();
-        int t = 0;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        minHeap.offer(new int[]{k, 0});
+
+        int[] distance = new int[n];
+        Arrays.fill(distance, INF);
+        // 从k出发
+        distance[k - 1] = 0;
+        // 0: weight / 1: v
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        minHeap.offer(new int[]{0, k - 1});
         while (!minHeap.isEmpty()) {
-            int[] node = minHeap.poll();
-            int v = node[0], weight = node[1];
-            if (visited.contains(v)) {
+            int[] p = minHeap.poll();
+            int w1 = p[0], v1 = p[1];
+            // 贪心策略，如果当前路径比出队路径更小，没有必要去更新周围邻居的最短路的，因为前值在出队之后，就已经把邻居的最短路都更新过了
+            if (distance[v1] < w1) {
                 continue;
             }
-            t = Math.max(t, weight);
-            visited.add(v);
-            List<int[]> neighbors = graph.get(v);
-            if (neighbors == null) {
-                continue;
-            }
-            for (int[] neighbor : neighbors) {
-                if (visited.contains(neighbor[0])) {
-                    continue;
+            for (int[] neighbor : graph[v1]) {
+                int v2 = neighbor[0], w2 = distance[v1] + neighbor[1];
+                if (w2 < distance[v2]) {
+                    distance[v2] = w2;
+                    minHeap.offer(new int[]{w2, v2});
                 }
-                minHeap.offer(new int[]{neighbor[0], weight + neighbor[1]});
             }
         }
-        return visited.size() == n ? t : -1;
+
+        int ans = Arrays.stream(distance).max().getAsInt();
+        return ans == INF ? -1 : ans;
     }
 
 
