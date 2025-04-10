d832e921d68b6460008be87572dd29a2eb8b4e0a
catxu <catxu@foxmail.com>
2025-04-10 23:34:49 +0800

    1116. Print Zero Even Odd (Semaphore)

--- com/catxu/leetcode/question1116/ZeroOddEven.java
+++ com/catxu/leetcode/question1116/ZeroOddEven.java
@@ -1,7 +1,6 @@
 package com.catxu.leetcode.question1116;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
 import java.util.function.IntConsumer;
 
 /**
@@ -9,70 +8,39 @@ import java.util.function.IntConsumer;
  */
 class ZeroEvenOdd {
     private final int n;
-
-    ReentrantLock lock = new ReentrantLock();
-    Condition enableZero = lock.newCondition();
-    Condition enableOdd = lock.newCondition();
-    Condition enableEven = lock.newCondition();
-    int flag = 0;
+    private final Semaphore zero = new Semaphore(1);
+    private final Semaphore even = new Semaphore(0);
+    private final Semaphore odd = new Semaphore(0);
 
     public ZeroEvenOdd(int n) {
         this.n = n;
     }
 
-    // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
         for (int i = 1; i <= n; i++) {
-            lock.lock();
-            try {
-                while (flag != 0) {
-                    enableZero.await();
-                }
-                printNumber.accept(0);
-                if (i % 2 == 0) {
-                    flag = 2;
-                    // print even
-                    enableEven.signalAll();
-                } else {
-                    flag = 1;
-                    // print odd
-                    enableOdd.signalAll();
-                }
-            } finally {
-                lock.unlock();
+            zero.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                even.release();
+            } else {
+                odd.release();
             }
         }
     }
 
     public void even(IntConsumer printNumber) throws InterruptedException {
         for (int i = 2; i <= n; i += 2) {
-            lock.lock();
-            try {
-                while (flag != 2) {
-                    enableEven.await();
-                }
-                printNumber.accept(i);
-                flag = 0;
-                enableZero.signalAll();
-            } finally {
-                lock.unlock();
-            }
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
         }
     }
 
     public void odd(IntConsumer printNumber) throws InterruptedException {
         for (int i = 1; i <= n; i += 2) {
-            lock.lock();
-            try {
-                while (flag != 1) {
-                    enableOdd.await();
-                }
-                printNumber.accept(i);
-                flag = 0;
-                enableZero.signalAll();
-            } finally {
-                lock.unlock();
-            }
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
         }
     }
 }
