5887a3abaede13401914a3e5f03eee6cff1fe006
yunqing <realyunq@gmail.com>
2025-04-25 10:00:15 +0800

    179. Largest Number

--- com/catxu/leetcode/question179/Solution.java
+++ com/catxu/leetcode/question179/Solution.java
@@ -37,5 +37,15 @@ class Solution {
 
     public static void main(String[] args) {
         System.out.println(new Solution().largestNumber(new int[]{5, 14, 7, 30, 2, 8}));
+        System.out.println(new Solution().largestNumber(new int[]{78, 5, 4}));
+
+        // 根据本题可以出一个镜像问题：
+        // 字符串 s 是由 n 个整数组成的最大数，给定字符串s，找到这 n 个整数，使其组成的和最小，返回 sum 值。
+        // 例1： s = "87530214", 输出 8 + 7 + 5 + 30 + 2 + 14 = 66
+        // 例2： s = "7854", 输出 78 + 5 + 4 = 87
+        // public int findLargestNumSum(String s)
+        // Constraints: s >= 0 且 s 是合法整数
+        // 从前往后遍历，遇到 s.charAt(i) > s.charAt(i + 1) 将 i 入队列 Queue
+        // 从前往后遍历，遇到 s.charAt(i) < s.charAt(i + 1) 将 i - 1 出队，然后加上当前数值重新入队
     }
 }
