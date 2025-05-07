4071a25c879fc87ba022435199d4117736883a04
yunqing <realyunq@gmail.com>
2025-05-07 16:18:49 +0800

    10. Regular Expression Matching

--- com/catxu/leetcode/question10/Solution.java
+++ com/catxu/leetcode/question10/Solution.java
@@ -1,9 +1,5 @@
 package com.catxu.leetcode.question10;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
 /**
  * 10. Regular Expression Matching
  * <p>
@@ -16,115 +12,78 @@ import java.util.Objects;
  * The matching should cover the entire input string (not partial).
  * <p>
  * Example 1:
- * <p>
+ * <pre>
  * Input: s = "aa", p = "a"
- * <p>
  * Output: false
- * <p>
  * Explanation: "a" does not match the entire string "aa".
- * <p>
+ * </pre>
  * Example 2:
- * <p>
+ * <pre>
  * Input: s = "aa", p = "a*"
- * <p>
  * Output: true
- * <p>
  * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
- * <p>
+ * </pre>
  * Example 3:
- * <p>
+ * <pre>
  * Input: s = "ab", p = ".*"
- * <p>
  * Output: true
- * <p>
  * Explanation: ".*" means "zero or more (*) of any character (.)".
- * <p>
+ * </pre>
  * Constraints:
- * <p>
+ * <pre>
  * 1 <= s.length <= 20
- * <p>
  * 1 <= p.length <= 20
- * <p>
  * s contains only lowercase English letters.
- * <p>
  * p contains only lowercase English letters, '.', and '*'.
- * <p>
  * It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+ * </pre>
  */
 class Solution {
     private char[] s;
     private char[] p;
 
-    private Map<Memoizer, Boolean> cache;
+    private Boolean[][] memo;
 
     public boolean isMatch(String s, String p) {
         this.s = s.toCharArray();
         this.p = p.toCharArray();
-        cache = new HashMap<>(this.s.length + this.p.length);
+        this.memo = new Boolean[s.length() + 1][p.length() + 1];
         return dfs(0, 0);
     }
 
     private boolean dfs(int i, int j) {
-        Memoizer memoizer = Memoizer.of(i, j);
-        if (cache.containsKey(memoizer)) {
-            return cache.get(memoizer);
+        if (memo[i][j] != null) {
+            return memo[i][j];
         }
-        if (i >= s.length && j >= p.length) {
+        if (i == s.length && j == p.length) {
             return true;
-        }
-        if (j >= p.length) {
+        } else if (j == p.length) {
             return false;
         }
-        // [i] maybe out of bound
-        boolean match = i < s.length && (s[i] == p[j] || p[j] == '.');
-        if (j + 1 < p.length && p[j + 1] == '*') {
-            // decision tree
-            cache.put(memoizer, dfs(i, j + 2) /* Dont use '*' */ || (match && dfs(i + 1, j))/* Use the '*' */);
-            return cache.get(memoizer);
-        }
-        if (match) {
-            cache.put(memoizer, dfs(i + 1, j + 1));
-            return cache.get(memoizer);
-        }
-        cache.put(memoizer, false);
-        return false;
-    }
-
-    static class Memoizer {
-        private int i;
-        private int j;
-
-        public Memoizer(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
-
-        public static Memoizer of(int i, int j) {
-            return new Memoizer(i, j);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof Memoizer) {
-                return ((Memoizer) obj).i == this.i && ((Memoizer) obj).j == this.j;
+        boolean match = i < s.length && (p[j] == '.' || p[j] == s[i]);
+        if (j + 1 < p.length && p[j + 1] == '*') { // 优先校验 [j+1] 是否为 '*'
+            if (!match) {
+                /* '*' 匹配 0 次 */
+                match = dfs(i, j + 2);
+            } else {
+                /* Use the '*' 使用 '*' 的前提是前值必须match */
+                match = dfs(i, j + 2) || dfs(i + 1, j);
             }
-            return false;
+            memo[i][j] = match;
+            return match;
+        } else if (match) {
+            memo[i][j] = match;
+            return dfs(i + 1, j + 1);
         }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(i) ^ Objects.hashCode(j);
-        }
+        memo[i][j] = match;
+        return memo[i][j];
     }
 
     public static void main(String[] args) {
-//        System.out.println(Objects.hash(0));
-//        System.out.println(Objects.hash(1));
-//        System.out.println(Objects.hash(1) ^ Objects.hash(0));
         Solution solution = new Solution();
         System.out.println(solution.isMatch("ab", ".*"));
         System.out.println(solution.isMatch("ab", ".*c"));
-//        System.out.println(solution.isMatch("abb", ".*abb"));
+        System.out.println(solution.isMatch("abb", ".*abb"));
 //        System.out.println(solution.isMatch("aab", "c*a*a*b*"));
 //        System.out.println(solution.isMatch("ab", ".*..."));
 //        System.out.println(solution.isMatch("aaaaaaaaaaaaaaaaaaaac", "a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*"));
