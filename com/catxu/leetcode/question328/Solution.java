6ef9c4eb9880784b4c0109ef21803615f6e4887a
catxu <catxu@foxmail.com>
2025-05-17 11:10:19 +0800

    328. Odd Even Linked List

---
+++ com/catxu/leetcode/question328/Solution.java
@@ -0,0 +1,31 @@
+package com.catxu.leetcode.question328;
+
+import com.catxu.leetcode.question.ListNode;
+
+/**
+ * 328. Odd Even Linked List
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
