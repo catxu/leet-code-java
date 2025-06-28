f58edae5804201e195217ddcbdc92db66effc052
catxu <catxu@foxmail.com>
2025-06-28 23:08:52 +0800

    2099. Find Subsequence of Length K With the Largest Sum

--- com/catxu/leetcode/question2099/Solution.java
+++ com/catxu/leetcode/question2099/Solution.java
@@ -1,6 +1,7 @@
 package com.catxu.leetcode.question2099;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * 2099. Find Subsequence of Length K With the Largest Sum
@@ -22,7 +23,28 @@ class Solution {
         return ans;
     }
 
+    public int[] maxSubsequenceII(int[] nums, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]); // max heap
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            pq.offer(new int[]{i, nums[i]});
+        }
+        int[] ans = new int[k];
+        int i = 0;
+        while (i != k) {
+            int[] e = pq.poll();
+            ans[i] = e[0];
+            i++;
+        }
+        Arrays.sort(ans); // 索引排序
+        for (i = 0; i < k; i++) {
+            ans[i] = nums[ans[i]];
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new Solution().maxSubsequence(new int[]{3, -1, 4, -2}, 3)));
+        System.out.println(Arrays.toString(new Solution().maxSubsequenceII(new int[]{3, -1, 4, -2}, 3)));
     }
 }
